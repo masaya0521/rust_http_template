6cb0a35d5b8138a3866eb21f2f57953a39df8220
masaya0521 <kaji0521km@gmail.com>
2023-07-26 00:03:06 +0900

    httpサーバの実装 なぜかServerがimportできない、、

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::Infallible;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello World!".into()))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let make_svc = make_service_fn(|_conn| 
+        async {Ok::<_, Infallible>(service_fn(handle_request))}
+    );
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Server running on http;//{}", addr);
+
+    server.await?;
+
+    Ok(())
 }
