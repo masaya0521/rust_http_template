dcfa0183715196731921ab5bf0c62c8b044a2355
masaya0521 <kaji0521km@gmail.com>
2023-07-28 00:08:00 +0900

    add オウム返しするechoパス追加 https://hyper.rs/guides/0.14/server/echo/

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
+use hyper::http::response;
 use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
 
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Method, StatusCode};
 
-async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello World!".into()))
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        }
+        (&Method::POST, "/echo") => *response.body_mut() = req.into_body(),
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Ok(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    pretty_env_logger::init();
     let addr = ([127, 0, 0, 1], 3000).into();
 
     let make_svc =
