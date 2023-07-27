71b198cdeaa0a74a5d131358e80770744f9a3e1b
masaya0521 <kaji0521km@gmail.com>
2023-07-27 23:19:45 +0900

    fix Serverがimportできなかったので修正 https://hyper.rs/guides/0.14/server/hello-world/ この通りにやった

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use hyper::{Body, Request, Response, Server};
 use std::convert::Infallible;
 
-use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 
 async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
@@ -11,9 +11,8 @@ async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallibl
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = ([127, 0, 0, 1], 3000).into();
 
-    let make_svc = make_service_fn(|_conn| 
-        async {Ok::<_, Infallible>(service_fn(handle_request))}
-    );
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
 
     let server = Server::bind(&addr).serve(make_svc);
 
